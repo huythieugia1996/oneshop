9adf3b1324a076d7a37b2df063425ce922d5732f
huynv <huynv.196@gmail.com>
2018-10-13 11:24:12 +0700

    first commit

---
+++ src/main/java/com/webbanhang/demo/OneshopApplication.java
@@ -0,0 +1,12 @@
+package com.webbanhang.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneshopApplication.class, args);
+    }
+}
